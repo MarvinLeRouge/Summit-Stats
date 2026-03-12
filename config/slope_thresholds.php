e8ba8cdcbdf503c3a70d4d9f22f269cb9a13ea06
Jean Ceugniet <jean.ceugniet@gmail.com>
2026-03-12 11:46:08 +0100

    feat(setup): init Laravel 12 + Vue 3 + Pest + structure dossiers + slope_thresholds config

---
+++ config/slope_thresholds.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'classes' => [
+        ['key' => 'lt5',   'label' => 'Plat',    'min' => null, 'max' => 5],
+        ['key' => '5_15',  'label' => 'Modéré',  'min' => 5,    'max' => 15],
+        ['key' => '15_25', 'label' => 'Pentu',   'min' => 15,   'max' => 25],
+        ['key' => '25_35', 'label' => 'Raide',   'min' => 25,   'max' => 35],
+        ['key' => 'gt35',  'label' => 'Extrême', 'min' => 35,   'max' => null],
+    ],
+];
